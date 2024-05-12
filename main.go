e2c54ed1ad6cfee57af2d8d170358e0f405d83a8
pengwin61 <pengwin08@gmail.com>
2024-05-12 17:55:13 +0300

    перехожу на новую библиотеку для получения старых ключей

--- main.go
+++ main.go
@@ -2,70 +2,104 @@ package main
 
 import (
 	"context"
-	"dotenv-builder/internal/core"
-	"flag"
+	"dotenv-builder/internal/vault"
 	"fmt"
-	"log"
-	"os"
-	"time"
 
-	"github.com/hashicorp/vault-client-go"
+	"log"
+
+	"github.com/hashicorp/vault/api"
 )
 
 func main() {
+	// ctx := context.Background()
+	vaultEnginesName := "secret"
+	metaPath := "metadata"
+	secretPath := "test/prj/prj1/development/"
 
-	// Получаем путь к секретам из флага
-	vaultSecretPath := flag.String("path", "projects/", "path to secrets")
-	if *vaultSecretPath == "" {
-		log.Fatal("path is empty, i can`t get secrets without path")
-	}
-	flag.Parse()
+	fullPath := vaultEnginesName + "/" + metaPath + "/" + secretPath
 
-	// Проверяем существует ли файл .env
-	core.CheckDotEnv(core.ENV_FILE)
+	// vault_addr := os.Getenv("VAULT_ADDR")
+	// vaultToken := os.Getenv("VAULT_TOKEN")
+	// vaultEnginesName := os.Getenv("VAULT_SECRETS_ENGINES_NAME")
 
-	ctx := context.Background()
-	vault_addr := os.Getenv("VAULT_ADDR")
-	vaultToken := os.Getenv("VAULT_TOKEN")
-	// vaultSecretPath := os.Getenv("VAULT_SECRET_PATH")
-
-	// создаем клиента vault
-	client, err := vault.New(
-		vault.WithAddress(vault_addr),
-		vault.WithRequestTimeout(30*time.Second),
-	)
+	vaultClient, err := vault.NewClient()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+	//
+	// sec := vaultClient.KVv2(vaultEnginesName)
+
+	//
+	liiis := getListSecrets(vaultClient, fullPath)
+
+	for _, v := range liiis {
+		fmt.Println(v)
+		// res := fmt.Sprintf("%s%s", "secret/test/prj/prj1/development", v)
+		// fmt.Println(res)
+		// targetVerson := getCurrentVersion(sec, ctx, res)
+
+		// fmt.Println(targetVerson)
 	}
 
-	// авторизуемся через токен
-	if err := client.SetToken(vaultToken); err != nil {
-		log.Fatal(err)
-	}
+	//
 
-	// получае список секретов
-	list, err := client.Secrets.KvV2List(ctx, *vaultSecretPath, vault.WithMountPath("secret"))
+	// vaultClient.SetToken(os.Getenv("VAULT_TOKEN"))
+
+	// targetVerson := getOldVersion(sec, ctx, path)
+	// getCurrentVersion(sec, ctx, path)
+	// getTagretVersion(sec, ctx, path, targetVerson)
+
+}
+
+func getOldVersion(sec *api.KVv2, ctx context.Context, path string) int {
+	list, err := sec.GetVersionsAsList(ctx, path)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+	}
+	oldVersion := len(list) - 1
+	fmt.Println("OLD VERSION:", oldVersion)
+
+	return oldVersion
+}
+
+func getCurrentVersion(sec *api.KVv2, ctx context.Context, path string) {
+	key, err := sec.Get(ctx, path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("---")
+	fmt.Println("CURRENT DATA")
+	for k, v := range key.Data {
+		fmt.Println(k, v)
+	}
+}
+
+func getTagretVersion(sec *api.KVv2, ctx context.Context, path string, version int) {
+	old, err := sec.GetVersion(ctx, path, version)
+	if err != nil {
+		fmt.Println(err)
 	}
 
-	for _, v := range list.Data.Keys {
-
-		secretPath := fmt.Sprint(*vaultSecretPath, "/", v)
-
-		core.WriteHeaders(v)
-
-		// читаем секреты
-		s, err := client.Secrets.KvV2Read(ctx, secretPath, vault.WithMountPath("secret"))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// записываем в файл .env
-		for k, v := range s.Data.Data {
-			core.WriteFileEnv(core.ENV_FILE, fmt.Sprintf("%s=%v\n", k, v))
-
-			// fmt.Printf("%s=%v\n", k, v)
-		}
+	fmt.Println("---")
+	fmt.Println("VERSION:", version)
+	for k, v := range old.Data {
+		fmt.Println(k, v)
 	}
+	fmt.Println("---")
+}
+
+func getListSecrets(vaultClient *api.Client, path string) []string {
+
+	var array []string
+	list, err := vaultClient.Logical().List(path)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for _, v := range list.Data {
+		a := fmt.Sprintln(v)
+		array = append(array, a)
+	}
+
+	return array
 }
